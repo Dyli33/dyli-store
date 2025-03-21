3e89f7d7271526997149aa7ed94bc425aac7a1c6
Dyli33 <dyli.prod@gmail.com>
2025-03-21 18:09:06 +0100

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,9 @@
+{
+    "redirects": [
+      { "source": "/:path.html", "destination": "/:path", "permanent": true }
+    ],
+    "rewrites": [
+      { "source": "/:path", "destination": "/:path.html" }
+    ]
+  }
+  
